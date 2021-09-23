c727d1f6afe8fa19fe6947887abb4add94760793
KarinAlbiez0910 <KarinAlbiez@gmail.com>
2021-09-23 16:53:56 +0200

    Update Binomialdistribution.py

--- prob_bin_gauss_distributions-0.1/prob_bin_gauss_distributions/Binomialdistribution.py
+++ prob_bin_gauss_distributions-0.1/prob_bin_gauss_distributions/Binomialdistribution.py
@@ -15,10 +15,7 @@ class Binomial(Distribution):
         stdev (float) representing the standard deviation of the distribution
         data_list (list of floats) a list of floats to be extracted from the data file
         p (float) representing the probability of an event occurring
-        n (int) the total number of trials
-    
-    
-    TODO: Fill out all TODOs in the functions below
+        n (int) the total number of trial
             
     """
         
